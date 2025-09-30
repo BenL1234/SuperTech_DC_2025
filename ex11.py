e46d6291efef3b8c23fb14766f1bb8c8346709bc
Donald Cameron <donald.cameron@qa.com>
2025-09-30 15:40:18 +0100

    Created starter for Ex11.py

---
+++ ex11.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'

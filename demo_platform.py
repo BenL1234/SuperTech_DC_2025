5df0189d5312d88102a86bb87f744a170caac61c
Donald Cameron <donald.cameron@qa.com>
2025-09-30 10:29:08 +0100

    Created demo for testing your platform the script is running on

---
+++ demo_platform.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo HOWTO check which platform your
+# program is running on..
+"""
+    Docstring:
+"""
+
+import sys
+import os
+
+my_home = None
+
+if sys.platform == "win32":
+    my_home = os.environ['HOMEPATH']
+elif sys.platform == "darwin" or sys.platform == "linux":
+    my_home = os.environ['HOME']
+else:
+    print("On some other OS")
+
+print("Home is", my_home)

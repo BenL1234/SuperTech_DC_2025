0fe8f65a88ba2bf91a44feaef5d94da60776625f
Donald Cameron <donald.cameron@qa.com>
2025-10-01 09:39:28 +0100

    Updated demo_file_seq_rw with iterator loop

--- demo_file_seq_rw.py
+++ demo_file_seq_rw.py
@@ -28,7 +28,16 @@ fh_in = open(r"c:\labs\projects\Sky_SuperTech_DC_2025\movies.txt", mode="rt")
 
 # text = fh_in.read() # Read ENTIRE file into a str object! Be Careful of LARGE files!
 # text = fh_in.read(30) # Read NEXT 30 chars into str object!
-text = fh_in.readline() # Read NEXT LINE into str object!
-print(text)
+# text = fh_in.readline() # Read NEXT LINE into str object!
+# lines = fh_in.readlines() # Read ENTIRE file into a LIST object! Be Careful!
+# print(f"1st Line = {lines[0]}")
+# print(f"Last Line = {lines[-1]}")
 
-fh_in.close()
+# ITERATE through the file handle
+# USING an ITERATOR for loop plus file_handle (Iterable Object = next/iter)
+# for line in open(r"c:\labs\projects\Sky_SuperTech_DC_2025\movies.txt", mode="rt"):
+for line in fh_in:
+    print(line, end="")
+
+# fh_in.flush() # Flush unwritten data
+fh_in.close() # Flush buffers and close file handle

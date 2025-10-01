db798fa771d59e0185e217ec5d485e757d1a8b13
Donald Cameron <donald.cameron@qa.com>
2025-10-01 12:32:27 +0100

    Created demo_words_user_function.py

---
+++ demo_words_user_function.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+def search_word(pattern=r"sage", file=r"c:\labs\words"):
+    lines_matched = 0
+    with (open(file, mode="rt") as fh_in):
+        for (line_num, line) in enumerate(fh_in, start=1):
+            if line.isascii() and pattern in line:
+                print(line_num, line, end="")
+                lines_matched += 1
+    return lines_matched
+
+total_lines = 0
+total_lines += search_word("done", r"c:\labs\words")
+print("-" * 50)
+total_lines += search_word()
+print("-" * 50)
+print(f"Total lines matched = {total_lines}")

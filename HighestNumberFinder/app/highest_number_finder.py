e44d370ba796512025a78817a253599b24d94526
Donald Cameron <donald.cameron@qa.com>
2025-10-02 16:58:17 +0100

    Added CODE to PASS 1st test to test for list of one result in first item

---
+++ HighestNumberFinder/app/highest_number_finder.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+class HighestNumberFinder:
+    def find_highest_number(self, numbers):
+        return numbers[0]

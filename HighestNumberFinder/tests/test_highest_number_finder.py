19e539a9d9e3ad01b97c9bd08e9364d8cc9aad4c
Donald Cameron <donald.cameron@qa.com>
2025-10-02 16:50:21 +0100

    Added 1st test to test for list of one result in first item

---
+++ HighestNumberFinder/tests/test_highest_number_finder.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo
+"""
+    Docstring:
+"""
+import unittest
+
+
+class TestHighestNumberFinder(unittest.TestCase):
+    def test_find_highest_in_list_one_result_single_item(self):
+        # Structure TestCases TDD - AAAs
+        # Arrange
+        input_value = [10]
+        expected_result = 10
+        cut = HighestNumberFinder() # Class Under Test
+
+        # Act
+        actual_result = cut.find_highest_number(input_value)
+
+        # Assert
+        self.assertEqual(actual_result, expected_result)
